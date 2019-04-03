162b16eb53fa30c07b27927e73f618ad286e56f6
Vladimir Mezentsov <vm35x@yahoo.com>
2019-04-03 08:49:49 -0400

    Update NotFound.js

--- imports/ui/NotFound.js
+++ imports/ui/NotFound.js
@@ -5,7 +5,7 @@ const NotFound = () => {
   return (
     <div className="boxed-view">
       <div className="boxed-view__box">
-        <h1>404 Page Not Found</h1>
+        <h1>404 - Page Not Found</h1>
         <p>Hmmm, we're unable to find that page.</p>
         <Link className="button button--link" to="/">Head Home</Link>
       </div>
